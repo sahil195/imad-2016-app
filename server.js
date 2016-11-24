d3a507120e090d5de40ebf7d7116cf95bfaa93a9
sahil195 <sahilbansal195@gmail.com>
2016-11-24 09:40:15 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/webappr', function (req, res) {
+app.get('/webapp', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'webapp.html'));
 });
 
